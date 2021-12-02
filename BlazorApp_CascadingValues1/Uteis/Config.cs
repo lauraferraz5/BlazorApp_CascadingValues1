34d2a9052537a28611f73b0a6f221412dbf1ee37
Laura Ferraz <laura@magnetcustomer.com>
2021-12-02 16:44:14 -0300

    Add project files.

---
+++ BlazorApp_CascadingValues1/Uteis/Config.cs
@@ -0,0 +1,12 @@
+﻿//uma forma de simplificar a passagem de parâmetros é passar um tipo complexo, como uma classe
+//pode definir "n" parâmetros e passar somente uma instância e usar apenas um CascadingValue (em MainLayout.razor)
+
+namespace BlazorApp_CascadingValues1.Uteis
+{
+    public class Config
+    {
+        public string CorDeFundo { get; set; } = "yellow";
+
+        public string TamanhoFonte { get; set; } = "40px";
+    }
+}
